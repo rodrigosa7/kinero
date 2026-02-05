a7cfe0d7ec6c5547fa891b9ffd2989205a9d9217
rodrigo.sa <rodrigo.gomes.sa@outlook.pt>
2026-02-05 20:28:06 +0000

    feat: init repo

---
+++ src/main/java/dev/kinero/kinerobackend/KineroApplication.java
@@ -0,0 +1,13 @@
+package dev.kinero.kinerobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KineroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KineroApplication.class, args);
+    }
+
+}
