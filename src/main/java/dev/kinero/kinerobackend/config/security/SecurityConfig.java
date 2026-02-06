ae171bdb37779cb8f653f49b25effc365f41f307
rodrigo.sa <rodrigo.gomes.sa@outlook.pt>
2026-02-06 00:09:41 +0000

    feat: add BCryptPasswordEncoder for password hashing

--- src/main/java/dev/kinero/kinerobackend/config/security/SecurityConfig.java
+++ src/main/java/dev/kinero/kinerobackend/config/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package dev.kinero.kinerobackend.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +23,10 @@ public class SecurityConfig {
                 .httpBasic(httpBasic -> httpBasic.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
